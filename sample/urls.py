17d9d479566d6e533f2b6797625e0b27fd15f73e
Aditya Maskar <aditya01092001@gmail.com>
2021-10-30 02:02:34 +0530

    second Commit

--- sample/urls.py
+++ sample/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.index, name="home"),
     path('result/', views.sample, name="home"),
+    path('student_info/', views.student, name="home"),
 ]
+
+
+
+
+
--- sample/views.py
+++ sample/views.py
@@ -1,14 +1,29 @@
 from django.shortcuts import render
 from .models import Student
+from .models import StudentList
 
 
 # Create your views here.
 
 
+def student(request):
+    if request.method == 'POST':
+        cont = StudentList.objects.filter(student_name=request.POST['your_name'])
+        check = ['Aditya']
+        extra_context = {'studentinfo': cont, 'check': check}
+        return render(request, 'student_info.html', extra_context)
+    return render(request, 'student_info.html')
+
+
+def index(request):
+    return render(request, 'home.html')
+
+
 def sample(request):
     if request.method == 'POST':
         context = Student.objects.filter(student_id=request.POST['stud_id'])
-        extra_context = {'student': context}
-        return render(request, 'home.html', extra_context)
+        check = ['Aditya']
+        extra_context = {'student': context, 'check': check}
+        return render(request, 'result.html', extra_context)
 
-    return render(request, 'home.html')
+    return render(request, 'result.html')
